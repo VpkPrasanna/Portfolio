b151b5598615426a68dee38b999b181cd43cbff3
VpkPrasanna <prasannakumarv@teknuance.com>
2020-04-10 19:06:05 +0530

    changes done on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,51 +1,35 @@
 import React from "react"
-import { graphql } from 'gatsby'
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroSection from "../components/Resuable/HeroSection"
 import Infoblock from "../components/Resuable/Inflblock"
 import Contact from "../components/contact/Contact"
 
-
-const ContactPage = ({data }) => (
+const ContactPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <HeroSection 
-      img = {data.img.childImageSharp.fluid}
-      title = "Contact Us"
-      subtitle = ""
-    heroclass ="about-background"/>
-    <Contact/>
-    <Infoblock heading = "How can we help"/>
+    <HeroSection
+      img={data.img.childImageSharp.fluid}
+      title="Contact Us"
+      subtitle=""
+      heroclass="about-background"
+    />
+    <Contact />
+    <Infoblock heading="How can we help" />
   </Layout>
 )
 
-
 export const query = graphql`
-{
-  img: file(relativePath: { eq: "contact.png" }) {
-    childImageSharp {
-      fluid(maxWidth: 400, maxHeight: 250) {
-        ...GatsbyImageSharpFluid_tracedSVG
-      }
-    }
-  }
-  mycourses: allContentfulCourses {
-    edges {
-      node {
-        id
-        title
-        description
-        price
-        category
-        image {
-          fixed(width:200,height:120) {
-            ...GatsbyContentfulFixed_tracedSVG
-          }
+  {
+    img: file(relativePath: { eq: "contact.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 400, maxHeight: 250) {
+          ...GatsbyImageSharpFluid_tracedSVG
         }
       }
     }
   }
-}`
+`
 
 export default ContactPage
